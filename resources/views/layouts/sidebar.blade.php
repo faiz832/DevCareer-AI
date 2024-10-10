2e042d1ffdcf6936a3761560c6f99d4682633284
Faiz <faqodefazz@gmail.com>
2024-10-10 09:40:15 +0700

    change backdrop color

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -148,7 +148,7 @@
     <div x-show="open" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0"
         x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-200"
         x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" @click="open = false"
-        class="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden">
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity z-40 lg:hidden">
     </div>
 
     <!-- Sidebar -->
