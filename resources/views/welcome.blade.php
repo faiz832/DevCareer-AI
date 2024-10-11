a9433f8364493cb0d8030b1b467a333320510ec7
Faiz <faqodefazz@gmail.com>
2024-10-11 18:54:26 +0700

    handle course image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,8 +133,9 @@
                 @foreach ($courses as $course)
                     <div
                         class="bg-white border rounded-lg p-3 hover:shadow-lg transition duration-300 ease-in-out flex flex-col justify-between">
-                        <img src="{{ Storage::url($course->thumbnail) }}" alt="{{ $course->name }}"
-                            class="w-full h-48 shadow-md object-cover rounded-lg mb-4" loading="lazy">
+                        <img src="{{ Storage::url($course->thumbnail) ?? 'https://via.placeholder.com/400x200' }}"
+                            alt="{{ $course->name }}" class="w-full h-48 shadow-md object-cover rounded-lg mb-4"
+                            loading="lazy">
                         <div class="flex flex-col flex-grow">
                             <h3 class="font-semibold text-xl mb-2">{{ $course->name }}</h3>
                             <div class="flex flex-col justify-between h-full">
