ab010a0d68910e48beb9054afea7dac8793a3720
Faiz <faqodefazz@gmail.com>
2024-10-12 08:03:48 +0700

    remove activity log in register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,14 +32,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'avatar' => ['nullable', 'image', 'mimes:png,jpg,jpeg'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         // Proses upload file photo kepada project laravel kita
         if ($request->hasFile('avatar')) {
             $avatarPath = $request->file('avatar')->store('avatars', 'public');
-        }else {
+        } else {
             $avatarPath = 'images/avatar-default.png';
         }
 
@@ -53,12 +53,6 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
-        CauserResolver::setCauser($user);
-        activity()
-            ->performedOn($user)
-            ->withProperties(['attributes' => $user->toArray()])
-            ->log('User registered');
 
         return redirect(route('dashboard', absolute: false));
     }
