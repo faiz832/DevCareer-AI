02dfb6dfe69f278f73b2466449e717a1b056e8a6
Faiz <faqodefazz@gmail.com>
2024-10-16 14:00:10 +0700

    fix style

--- resources/views/admin/transactions/index.blade.php
+++ resources/views/admin/transactions/index.blade.php
@@ -19,7 +19,7 @@
                                         Account</th>
                                     <th
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Course</th>
+                                        Amount</th>
                                     <th
                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Date</th>
